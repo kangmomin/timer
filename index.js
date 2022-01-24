a35ecac9595589f932a505d810dc498b9eb507aa
koldin108902 <momin108902@naver.com>
2022-01-24 18:12:13 +0900

    console.time보다 0.0001초가량 빨라짐

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ class Timer {
                 this.isEnd = false
             },
             start() {
-                const date = new Date()
-                this.startTime = (date.getTime() + date.getMilliseconds()/1000)
+                this.startTime = performance.now()
                 this.isEnd = false
             },
             end() {
-                const date = new Date()
-                this.endTime = (date.getTime() + date.getMilliseconds())
+                this.endTime = performance.now()
                 this.time = (this.endTime - this.startTime) / 1000
+                // console.log(this.time)
                 this.isEnd = true
             },
             startTime: 0,
@@ -24,59 +23,4 @@ class Timer {
             isEnd: false
         }
     }
-}
-
-main()
-
-function main() {
-    const testList = [1, 21, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-    const pure_for = new Timer()
-    const for_of = new Timer()
-    const for_each = new Timer()
-
-    pure_for.init()
-    for_each.init()
-    for_of.init()
-    
-    // pure_for.start()
-    // pureFor(testList)
-    // pure_for.end()
-    
-    // console.time("pure")
-    // pureFor(testList)
-    // console.timeEnd("pure")
-    
-    // console.log(`pure for: ${pure_for.time}`)
-    
-    test()[0]()
-    test()[1]()
-    
-    function test() {
-        return [
-        () => {
-            for_of.start()
-            setTimeout(() => {
-                for_of.end()        
-                console.log(`for of: ${for_of.time}`)
-            }, 1000)
-        },
-        () => {
-            console.time("a")
-            setTimeout(() => {
-                console.timeEnd("a")
-            }, 1000)
-        }]
-    }
-}
-
-function pureFor(testList) {
-    for (let i = 0; i < testList; i++) {
-        const x = testList[i]
-    }
-}
-function forOf(testList) {
-    for (test of testList) {}
-}
-function forEach(testList) {
-    testList.forEach()
 }
